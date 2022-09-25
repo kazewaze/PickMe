558dc71b73a1e412c616b2372fff8eaf754c02ae
Kaycee Ingram <KayceeWilliamIngram@gmail.com>
2022-09-24 18:45:17 -0700

    fixed silly mistake on randomizing engine

--- utils/random.js
+++ utils/random.js
@@ -1,19 +1,19 @@
 /*
 * The PickMe! Randomizing Engine - Version 1.0.0
-* Copyright 2022 - Kaycee Ingram <kazewaze>
+* Copyright 2022... jk ;) - Kaycee Ingram <kazewaze>
 */
 
 export default function choose(array_) {
-  let counter = [0, 0, 0];
+  let counter = Array(array_.length).fill(0);
 
   do {
-   counter[array_.indexOf(random(array_))]++;
+   counter[array_.indexOf(randomSelection(array_))]++;
   } while (!counter.includes(3));
 
   return array_[counter.indexOf(3)];
 }
 
-function random(array_) {
+function randomSelection(array_) {
   return array_[randomIdx(array_.length)];
 }
 
