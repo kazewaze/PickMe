6f1806c7422ad77756fe89b0fa894715c68f47f6
Kaycee Ingram <KayceeWilliamIngram@gmail.com>
2022-10-08 17:48:00 -0700

    added new random algorithm for better results than math.random

--- utils/randomButBetter.js
+++ utils/randomButBetter.js
@@ -4,15 +4,20 @@
 * Copyright 2022... jk ;) - Kaycee Ingram <kazewaze>
 */
 
+const MersenneTwister = require('./mersenne_twister');
+const mtAlgo = new MersenneTwister();
+
 export default function choose(array_) {
-  let [chosenHistory, status] = [[], false];
+  let [chosenHistory, status, numberOfDuplicates] = [[], false, 7];
   array_ = array_.filter(selection => selection !== "");
 
   do {
+    chosenHistory = chosenHistory.length >= 2 && !allEqual(chosenHistory) ? [] : chosenHistory;
+
     chosenHistory.push(randomSelection(array_));
 
-    if (chosenHistory.length === 7) {
-      if (repeatDuplicates(chosenHistory, 7)) {
+    if (chosenHistory.length === numberOfDuplicates) {
+      if (repeatDuplicates(chosenHistory, numberOfDuplicates)) {
         status = true;
       } else {
         chosenHistory = [];
@@ -28,7 +33,11 @@ function randomSelection(array_) {
 }
 
 function randomIdx(arrayLength_) {
-  return Math.floor(Math.random() * arrayLength_);
+  return Math.floor(mtAlgo.random() * arrayLength_);
+}
+
+function allEqual(array_) {
+  return array_.every(idx => idx === array_[0] ? true : false);
 }
 
 function repeatDuplicates(arr, amount=3) {
@@ -47,3 +56,11 @@ function repeatDuplicates(arr, amount=3) {
     }
   }
 }
+
+// function evenOrOdd(num) {
+//   return num % 2 === 0;
+// }
+
+// console.log(Math.floor(mtAlgo.random() * 5));
+
+// console.log(choose['k', 'w', 'i']);
